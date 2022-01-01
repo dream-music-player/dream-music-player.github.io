35590125894e0424f30e093fdba1b1948be3514c
Anonymous <>
2021-12-31 21:39:24 -0800

    Update

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,1 @@
-self.addEventListener('fetch', function(event) {
-    event.respondWith(async function() {
-        try {
-            var res = await fetch(event.request);
-            var cache = await caches.open('cache');
-            cache.put(event.request.url, res.clone());
-            return res;
-        } catch (error) {
-            return caches.match(event.request);
-        }
-    }());
-});
+self.addEventListener("fetch",function(e){e.respondWith(async function(){try{var t=await fetch(e.request);return(await caches.open("cache")).put(e.request.url,t.clone()),t}catch(t){return caches.match(e.request)}}())});
